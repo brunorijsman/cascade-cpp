340bfb66b9039370e701f4d46b27232b468246e5
Bruno Rijsman <brunorijsman@gmail.com>
2020-04-06 15:50:07 -0600

    put caching back

--- cascade/algorithm.cpp
+++ cascade/algorithm.cpp
@@ -79,7 +79,7 @@ Algorithm original_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: Cascade mod. (1)
 // Name in Andre Reis Thesis : biconf
@@ -106,7 +106,7 @@ Algorithm biconf_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: -
 // Name in Andre Reis Thesis : -
@@ -121,7 +121,7 @@ Algorithm biconf_cascade_algorithm(
     true,                                   // biconf_cascade    
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: -
 // Name in Andre Reis Thesis : -
@@ -136,7 +136,7 @@ Algorithm biconf_complement_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: Cascade opt. (2)
 // Name in Andre Reis Thesis : yanetal (Yan et al.)
@@ -167,8 +167,7 @@ Algorithm yanetal_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    //@@@
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: Cascade opt. (3)
 // Name in Andre Reis Thesis : -
@@ -199,7 +198,7 @@ Algorithm option3_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: Cascade opt. (4)
 // Name in Andre Reis Thesis : -
@@ -214,7 +213,7 @@ Algorithm option4_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Note: Cascade opt. (5) from the Demystifying paper is not supported yet:
 // Need to add support for deterministic shuffling
@@ -251,7 +250,7 @@ Algorithm option7_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles
 
 // Name in Demystifying paper: Cascade opt. (8)
 // Name in Andre Reis Thesis : option-8
@@ -285,4 +284,4 @@ Algorithm option8_algorithm(
     false,                                  // biconf_cascade
     false,                                  // block_parity_inference
     true,                                   // ask_correct_parity_using_shuffle_seed
-    false);                                  // cache_shuffles
+    true);                                  // cache_shuffles

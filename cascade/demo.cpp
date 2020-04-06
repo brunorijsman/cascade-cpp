a986299c187e2a3ca165222c5258db78b7c5ae1e
Bruno Rijsman <brunorijsman@gmail.com>
2020-04-06 16:15:20 -0600

    Add demo

--- cascade/demo.cpp
+++ cascade/demo.cpp
@@ -7,7 +7,11 @@ int main()
     std::mt19937 mt(rd());
     std::uniform_int_distribution<int> dist(0, 10000);
 
-    mt.seed(12345);
+    int seed;
+    std::cout << "Enter seed: ";
+    std::cin >> seed;
+    mt.seed(seed);
+
     for (int i=0; i<10; ++i) {
         std::cout << dist(mt) << std::endl;
     }
